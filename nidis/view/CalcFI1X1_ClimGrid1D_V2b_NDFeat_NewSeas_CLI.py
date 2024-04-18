bfb2e7ebe2b325f674fa48d7a6afef63755cd850
jordancaraballo <jordan.a.caraballo-vega@nasa.gov>
2024-04-18 01:03:39 -0400

    Fixing new mode of API

--- nidis/view/CalcFI1X1_ClimGrid1D_V2b_NDFeat_NewSeas_CLI.py
+++ nidis/view/CalcFI1X1_ClimGrid1D_V2b_NDFeat_NewSeas_CLI.py
@@ -181,8 +181,13 @@ def run_postprocessing(
         # if everything looks good, generate single file with outputs
         logging.info(f'Running output combination for {season}')
         CombineMultipleFilesIntoSingle(
-            indicator, season, indicator_output_dir,
-            postprocessed_output_dir, n_pixels)
+            indicator,
+            season,
+            indicator_output_dir,
+            postprocessed_output_dir,
+            netcdf_filename,
+            n_pixels
+        )
 
         # netcdf creation
         # run_create_netcdf()
