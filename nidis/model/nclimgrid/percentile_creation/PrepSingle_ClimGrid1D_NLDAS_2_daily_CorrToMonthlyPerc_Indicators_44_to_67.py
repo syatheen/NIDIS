774019d757a70fffb92478cf61a9a13918c2058b
jordancaraballo <jordan.a.caraballo-vega@nasa.gov>
2024-05-09 21:16:42 -0400

    Weekly resolution

---
+++ nidis/model/nclimgrid/percentile_creation/PrepSingle_ClimGrid1D_NLDAS_2_daily_CorrToMonthlyPerc_Indicators_44_to_67.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+# Coded by Soni Yatheendradas
+#         on Feb 23, 2023
+from __future__ import division
+import os
+import sys
+from datetime import date, datetime, timedelta
+import numpy as np
+from scipy.interpolate import interp1d
+from scipy.stats import rankdata
+#from CreateMonthlyLists_YYYYMMDDAndArray_File import CreateMonthlyLists_YYYYMMDDAndArray
+#from PrepTrainPortion_ClimDivs_MonthlyPercBased_File import PrepTrainPortion_ClimDivs_MonthlyPercBased
+# from PrintInfoAboutArray_File import PrintInfoAboutArray
+from nidis.model.nclimgrid.percentile_creation.Utils import \
+  CreateMonthlyLists_YYYYMMDDAndArray, \
+  PrepTrainPortion_ClimDivs_MonthlyPercBased, \
+  PrintInfoAboutArray
+
+#NOTE: sys.argv indices start at 1, not 0
+#Python arguments to this program will be (for now):
+#         ArgLSM ArgVariable ArgHUC
+#ArgNum   1       2           3
+
+
+
+def main(ArgLSM='Mosaic', ArgVariable='EVAP', ArgHUC='NA'):
+
+  # BEGIN code arguments / editable section
+
+  SingleUnified_BeginDateVecList = [2006, 1, 3] # Beginning single-unified year, month, day of month, this is also a Tuesday
+  SingleUnified_EndDateVecList = [2021, 8, 31] # Ending single-unified year, month, day of month, this is also a Tuesday
+
+  #ArgLSM = sys.argv[1] # Choices are 'Mosaic', 'Noah', 'SAC', 'VIC'
+  #ArgVariable = sys.argv[2] # Choices currently implemented are '1MSM', 'TCSM'
+  #ArgHUC = sys.argv[3] # Choices currently implemented are 'NA' (for "Not Applicable")
+
+  input_path_weekly = '/discover/nobackup/projects/nca/jacaraba/NIDIS_Data/RefArrays'
+  os.makedirs(input_path_weekly, exist_ok=True)
+
+  output_path_percentile = '/discover/nobackup/projects/nca/jacaraba/NIDIS_Data/PreppedTrainNEvalNpzs/ClimGrid1D'
+  os.makedirs(output_path_percentile, exist_ok=True)
+
+  if (ArgVariable == 'STRM') :
+
+    NLDAS_2_daily_RefFileName = os.path.join(
+      input_path_weekly,
+      'ClimGrid1D_' + ArgLSM + '_' + ArgVariable + '_' + ArgHUC + '_19800101To20210831.npz'
+    )
+
+    SingleUnifiedDataFilename = os.path.join(
+      output_path_percentile,
+      'SingleUnified_NLDAS_2_dly_' + ArgLSM + '_' + ArgVariable + '_' + ArgHUC + '_Crr2MnthlyPrc_' + str(SingleUnified_BeginDateVecList[0]) + format(SingleUnified_BeginDateVecList[1],'02') + format(SingleUnified_BeginDateVecList[2],'02') + 'To' + str(SingleUnified_EndDateVecList[0]) + format(SingleUnified_EndDateVecList[1],'02') + format(SingleUnified_EndDateVecList[2],'02') + '.npz'
+    )
+
+  else:
+
+    NLDAS_2_daily_RefFileName = os.path.join(
+      input_path_weekly,
+      'ClimGrid1D_' + ArgLSM + '_' + ArgVariable + '_19800101To20210831.npz'
+    )
+
+    SingleUnifiedDataFilename = os.path.join(
+      output_path_percentile,
+      'SingleUnified_NLDAS_2_dly_' + ArgLSM + '_' + ArgVariable + '_Crr2MnthlyPrc_' + str(SingleUnified_BeginDateVecList[0]) + format(SingleUnified_BeginDateVecList[1],'02') + format(SingleUnified_BeginDateVecList[2],'02') + 'To' + str(SingleUnified_EndDateVecList[0]) + format(SingleUnified_EndDateVecList[1],'02') + format(SingleUnified_EndDateVecList[2],'02') + '.npz'
+    )
+
+  #end of if (ArgVariable == 'STRM') :
+
+  # END code arguments / editable section
+
+  ssstart_Overall = datetime.now()
+
+  SingleUnified_BeginDate = date(SingleUnified_BeginDateVecList[0], SingleUnified_BeginDateVecList[1], SingleUnified_BeginDateVecList[2])
+  SingleUnified_EndDate = date(SingleUnified_EndDateVecList[0], SingleUnified_EndDateVecList[1], SingleUnified_EndDateVecList[2])
+
+  #BEGIN check whether the beginning and ending days are indeed Tuesdays
+  if SingleUnified_BeginDate.weekday() != 1:  # 0 for Monday, 1 for Tuesday
+    print('Beginning Date Vector for single-unified needs to be a Tuesday!!')
+    sys.exit(0)
+  if SingleUnified_EndDate.weekday() != 1:  # 0 for Monday, 1 for Tuesday
+    print('Ending Date Vector for single-unified needs to be a Tuesday!!')
+    sys.exit(0)
+  #END check whether the beginning and ending days are indeed Tuesdays
+
+  if SingleUnified_BeginDate > SingleUnified_EndDate:
+    print('SingleUnified_BeginDate should not be later than SingleUnified_EndDate!!!')
+    sys.exit(0)
+
+  NLDAS_2_daily_RefObject = np.load(NLDAS_2_daily_RefFileName)
+
+  if ArgVariable == '1MSM':
+    if ArgLSM == 'Mosaic':
+      NLDAS_2_daily_YYYYMMDD_Of_RefArray = NLDAS_2_daily_RefObject['Mosaic_1MSM_YYYYMMDD_Of_RefArray']
+      NLDAS_2_daily_RefArray = NLDAS_2_daily_RefObject['Mosaic_1MSM_RefArray']
+    elif ArgLSM == 'Noah':
+      NLDAS_2_daily_YYYYMMDD_Of_RefArray = NLDAS_2_daily_RefObject['Noah_1MSM_YYYYMMDD_Of_RefArray']
+      NLDAS_2_daily_RefArray = NLDAS_2_daily_RefObject['Noah_1MSM_RefArray']
+    elif ArgLSM == 'SAC':
+      NLDAS_2_daily_YYYYMMDD_Of_RefArray = NLDAS_2_daily_RefObject['SAC_1MSM_YYYYMMDD_Of_RefArray']
+      NLDAS_2_daily_RefArray = NLDAS_2_daily_RefObject['SAC_1MSM_RefArray']
+    elif ArgLSM == 'VIC':
+      NLDAS_2_daily_YYYYMMDD_Of_RefArray = NLDAS_2_daily_RefObject['VIC_1MSM_YYYYMMDD_Of_RefArray']
+      NLDAS_2_daily_RefArray = NLDAS_2_daily_RefObject['VIC_1MSM_RefArray']
+    #end of if ArgLSM == 'Mosaic'
+  elif ArgVariable == 'TCSM':
+    if ArgLSM == 'Mosaic':
+      NLDAS_2_daily_YYYYMMDD_Of_RefArray = NLDAS_2_daily_RefObject['Mosaic_TCSM_YYYYMMDD_Of_RefArray']
+      NLDAS_2_daily_RefArray = NLDAS_2_daily_RefObject['Mosaic_TCSM_RefArray']
+    elif ArgLSM == 'Noah':
+      NLDAS_2_daily_YYYYMMDD_Of_RefArray = NLDAS_2_daily_RefObject['Noah_TCSM_YYYYMMDD_Of_RefArray']
+      NLDAS_2_daily_RefArray = NLDAS_2_daily_RefObject['Noah_TCSM_RefArray']
+    elif ArgLSM == 'SAC':
+      NLDAS_2_daily_YYYYMMDD_Of_RefArray = NLDAS_2_daily_RefObject['SAC_TCSM_YYYYMMDD_Of_RefArray']
+      NLDAS_2_daily_RefArray = NLDAS_2_daily_RefObject['SAC_TCSM_RefArray']
+    elif ArgLSM == 'VIC':
+      NLDAS_2_daily_YYYYMMDD_Of_RefArray = NLDAS_2_daily_RefObject['VIC_TCSM_YYYYMMDD_Of_RefArray']
+      NLDAS_2_daily_RefArray = NLDAS_2_daily_RefObject['VIC_TCSM_RefArray']
+  elif ArgVariable == 'EVAP':
+    NLDAS_2_daily_YYYYMMDD_Of_RefArray = NLDAS_2_daily_RefObject[f'{ArgLSM}_EVAP_YYYYMMDD_Of_RefArray']
+    NLDAS_2_daily_RefArray = NLDAS_2_daily_RefObject[f'{ArgLSM}_EVAP_RefArray']
+  elif ArgVariable == 'SWE':
+    NLDAS_2_daily_YYYYMMDD_Of_RefArray = NLDAS_2_daily_RefObject[f'{ArgLSM}_SWE_YYYYMMDD_Of_RefArray']
+    NLDAS_2_daily_RefArray = NLDAS_2_daily_RefObject[f'{ArgLSM}_SWE_RefArray']
+  elif ArgVariable == 'RUN':
+    NLDAS_2_daily_YYYYMMDD_Of_RefArray = NLDAS_2_daily_RefObject[f'{ArgLSM}_RUN_YYYYMMDD_Of_RefArray']
+    NLDAS_2_daily_RefArray = NLDAS_2_daily_RefObject[f'{ArgLSM}_RUN_RefArray']
+  elif ArgVariable == 'STRM':
+    NLDAS_2_daily_YYYYMMDD_Of_RefArray = NLDAS_2_daily_RefObject[f'{ArgLSM}_STRM_YYYYMMDD_Of_RefArray']
+    NLDAS_2_daily_RefArray = NLDAS_2_daily_RefObject[f'{ArgLSM}_STRM_RefArray']
+  else: #of if ArgVariable == '1MSM'
+    print('Add remaining np.savez_compressed code lines for other variables!!')
+    sys.exit(0)
+  #end of if ArgVariable == '1MSM'
+
+  MonthlyList_NLDAS_2_daily_YYYYMMDD_Of_RefArray, MonthlyList_NLDAS_2_daily_RefArray = CreateMonthlyLists_YYYYMMDDAndArray(NLDAS_2_daily_YYYYMMDD_Of_RefArray, NLDAS_2_daily_RefArray)
+
+  #BEGIN section for single-unified
+
+  NLDAS_2_daily_YYYYMMDD_Of_PrcntlArray, NLDAS_2_daily_PrcntlArray = PrepTrainPortion_ClimDivs_MonthlyPercBased(NLDAS_2_daily_YYYYMMDD_Of_RefArray, NLDAS_2_daily_RefArray, SingleUnified_BeginDateVecList, SingleUnified_EndDateVecList, MonthlyList_NLDAS_2_daily_RefArray)
+
+  print('Single-unified: NumDates = ', NLDAS_2_daily_PrcntlArray.shape[0], ', NumSpatialUnits = ',NLDAS_2_daily_PrcntlArray.shape[1])
+
+  PrintInfoAboutArray(NLDAS_2_daily_YYYYMMDD_Of_PrcntlArray, 'NLDAS_2_daily_YYYYMMDD_Of_PrcntlArray')
+
+  PrintInfoAboutArray(NLDAS_2_daily_PrcntlArray, 'NLDAS_2_daily_PrcntlArray')
+
+  np.savez_compressed(SingleUnifiedDataFilename, 
+                      YYYYMMDD_Of_Array = NLDAS_2_daily_YYYYMMDD_Of_PrcntlArray, 
+                      NLDAS_2_daily_PrcntlArray = NLDAS_2_daily_PrcntlArray)
+
+  #END section for single-unified
+
+  eeend_Overall = datetime.now()
+  eeelapsed_Overall = eeend_Overall - ssstart_Overall
+  print(eeelapsed_Overall.seconds,"sec:",eeelapsed_Overall.microseconds,"microsec")
+
+  return
+
+if __name__ == '__main__':
+    main()
---
+++ nidis/model/nclimgrid/percentile_creation/Utils.py
@@ -0,0 +1,147 @@
+from __future__ import division
+from datetime import date, datetime, timedelta
+import sys
+import numpy as np
+from scipy.stats import rankdata
+
+def PrintInfoAboutArray(ThisArray, VariabNameStr):
+    print('')
+    print(VariabNameStr,".shape is ",ThisArray.shape)
+    print(VariabNameStr,".dtype is ",ThisArray.dtype)
+    print(VariabNameStr," is ",ThisArray)
+    print("np.amin(np.isnan(",VariabNameStr,").sum(axis=0)) is ",np.amin(np.isnan(ThisArray).sum(axis=0)))
+    print("np.amax(np.isnan(",VariabNameStr,").sum(axis=0)) is ",np.amax(np.isnan(ThisArray).sum(axis=0)))
+    print("np.amin(np.isnan(",VariabNameStr,").sum(axis=1)) is ",np.amin(np.isnan(ThisArray).sum(axis=1)))
+    print("np.amax(np.isnan(",VariabNameStr,").sum(axis=1)) is ",np.amax(np.isnan(ThisArray).sum(axis=1)))
+    print("np.isnan(np.nansum(ThisArray, axis = 0)).sum() is ",np.isnan(np.nansum(ThisArray, axis = 0)).sum())
+    print("np.isnan(np.nansum(ThisArray, axis = 1)).sum() is ",np.isnan(np.nansum(ThisArray, axis = 1)).sum())
+    print('overall min is ',np.nanmin(ThisArray),', overall max is ',np.nanmax(ThisArray))
+    print('')
+    return
+
+def FindPercentilesForValues(RefArray_1d, Values_1d):
+  RefArray_1d_NotNaN = RefArray_1d[~np.isnan(RefArray_1d)]
+  Values_1d_NotNaN = Values_1d[~np.isnan(Values_1d)]
+  if RefArray_1d_NotNaN.size > 0:
+    if Values_1d_NotNaN.size > 0:
+      SortedRef_1d = np.sort(RefArray_1d_NotNaN)
+      SortedRef_1d_size = SortedRef_1d.size
+      QuantileInverses_OrdinalTieRanks = (rankdata(SortedRef_1d, method='ordinal') - 1) / \
+                                         float(SortedRef_1d_size - 1)
+      QuantileInverses_AverageTieRanks = (rankdata(SortedRef_1d, method='average') - 1) / \
+                                         float(SortedRef_1d_size - 1)
+      Percentiles_1d = np.interp(Values_1d, SortedRef_1d, QuantileInverses_OrdinalTieRanks)
+      IfValuesInSortedRef = np.isin(Values_1d, SortedRef_1d)
+      ValuesInSortedRef = Values_1d[np.where(IfValuesInSortedRef)]
+      Idxs_ValuesInSortedRef_In_SortedRef = np.searchsorted(SortedRef_1d, ValuesInSortedRef)
+      Percentiles_AverageTieRanks = QuantileInverses_AverageTieRanks[Idxs_ValuesInSortedRef_In_SortedRef]
+      Percentiles_1d[np.where(IfValuesInSortedRef)] = Percentiles_AverageTieRanks
+      return Percentiles_1d
+    else: # if Values_1d_NotNaN.size > 0
+      return Values_1d
+    #end of if Values_1d_NotNaN.size > 0
+  else: # if RefArray_1d_NotNaN.size > 0
+    print('RefArray_1d_NotNaN.size == 0!!!')
+    if Values_1d_NotNaN.size > 0:
+      print('Values_1d_NotNaN.size > 0 when RefArray_1d_NotNaN.size == 0!!')
+      sys.exit(0)
+    else: # if Values_1d_NotNaN.size > 0
+      return Values_1d
+    #end of if Values_1d_NotNaN.size > 0
+  #end of if RefArray_1d_NotNaN.size > 0
+#end of def FindPercentilesForValues(RefArray_1d, Values_1d):
+
+def LoopPercentileCalcOverSpatialUnits(RefArray, PrcntlArray):
+  for WhichSpatialUnit in range(PrcntlArray.shape[1]):
+    RefArray_ThisSpatialUnit = RefArray[:, WhichSpatialUnit]
+    PrcntlArray_ThisSpatialUnit = PrcntlArray[:, WhichSpatialUnit]
+    PrcntlArray_ThisSpatialUnit = FindPercentilesForValues(RefArray_ThisSpatialUnit, PrcntlArray_ThisSpatialUnit)
+    PrcntlArray[:, WhichSpatialUnit] = PrcntlArray_ThisSpatialUnit
+  #end of for WhichSpatialUnit in range(PrcntlArray.shape[1]):
+  return PrcntlArray
+#end of def LoopPercentileCalcOverSpatialUnits(RefArray, PrcntlArray)
+
+def LoopPrcntlCalcOverMonthsNSpatialUnits(MonthlyList_RefArray, MonthlyList_PrcntlArray):
+  for WhichMonth in range(1,12+1):
+    RefArray = MonthlyList_RefArray[WhichMonth-1]
+    PrcntlArray = MonthlyList_PrcntlArray[WhichMonth-1]
+    PrcntlArray = LoopPercentileCalcOverSpatialUnits(RefArray, PrcntlArray)
+    MonthlyList_PrcntlArray[WhichMonth-1] = PrcntlArray
+  #end of for WhichMonth in range(1,12+1):
+  return MonthlyList_PrcntlArray
+#end of def LoopPrcntlCalcOverMonthsNSpatialUnits(MonthlyList_RefArray, MonthlyList_PrcntlArray)
+
+def CreateMonthlyLists_YYYYMMDDAndArray(YYYYMMDD_Of_Array, ThisArray):
+  MM_Of_Array = (YYYYMMDD_Of_Array % 10000) // 100
+  MonthlyList_YYYYMMDD_Of_Array = []
+  MonthlyList_Array = []
+  for WhichMonth in range(1,12+1):
+    Idxs = np.where(MM_Of_Array == WhichMonth)
+    YYYYMMDD_Of_Array_ThisMonth = YYYYMMDD_Of_Array[Idxs[0]]
+    ThisArray_ThisMonth = ThisArray[Idxs[0]]
+    MonthlyList_YYYYMMDD_Of_Array.append(YYYYMMDD_Of_Array_ThisMonth)
+    MonthlyList_Array.append(ThisArray_ThisMonth)
+  #end of for WhichMonth in range(1,12+1):
+  return MonthlyList_YYYYMMDD_Of_Array, MonthlyList_Array
+#end of def CreateMonthlyLists_YYYYMMDDAndArray(YYYYMMDD_Of_Array, ThisArray)
+
+def CreateYYYYMMDD_Of_Array_FromEndpointdates(BeginDate, EndDate):
+  TotalNumDaysDiff = abs(EndDate-BeginDate).days
+  TotalNumWeeksDiff = TotalNumDaysDiff//7
+  HumanDatesList_Of_Array = []
+  for NumWeeksDiff in range(0,TotalNumWeeksDiff+1):
+    IntermediateDate = BeginDate + timedelta(weeks=NumWeeksDiff)
+    HumanDatesList_Of_Array.append(10000*IntermediateDate.year + 100*IntermediateDate.month + IntermediateDate.day)
+  YYYYMMDD_Of_Array = np.array(HumanDatesList_Of_Array, dtype = np.int32)
+  YYYYMMDD_Of_Array = np.expand_dims(YYYYMMDD_Of_Array, axis=1)
+  return YYYYMMDD_Of_Array
+#end of def CreateYYYYMMDD_Of_Array_FromEndpointdates(BeginDate, EndDate)
+
+def TimeSlice_YYYYMMDDAndRefArrays(YYYYMMDD_Of_RefArray, RefArray, BeginDateVecList, EndDateVecList):
+  BeginYYYYMMDD = 10000*BeginDateVecList[0] + 100*BeginDateVecList[1] + BeginDateVecList[2]
+  EndYYYYMMDD = 10000*EndDateVecList[0] + 100*EndDateVecList[1] + EndDateVecList[2]
+  if ( ( BeginYYYYMMDD in list(YYYYMMDD_Of_RefArray) ) and
+       ( EndYYYYMMDD in list(YYYYMMDD_Of_RefArray) ) ) :
+    BeginIdx = list(YYYYMMDD_Of_RefArray).index(10000*BeginDateVecList[0] + 100*BeginDateVecList[1] + BeginDateVecList[2])
+    EndIdx = list(YYYYMMDD_Of_RefArray).index(10000*EndDateVecList[0] + 100*EndDateVecList[1] + EndDateVecList[2])
+    YYYYMMDD_Of_PrcntlArray = YYYYMMDD_Of_RefArray[ BeginIdx : EndIdx+1 ]
+    PrcntlArray = RefArray[ BeginIdx : EndIdx+1 ]
+  else: # if ( (BeginYYYYMMDD in list(YYYYMMDD_Of_RefArray) and...
+    BeginDate = date(BeginDateVecList[0], BeginDateVecList[1], BeginDateVecList[2])
+    EndDate = date(EndDateVecList[0], EndDateVecList[1], EndDateVecList[2])
+    YYYYMMDD_Of_PrcntlArray = CreateYYYYMMDD_Of_Array_FromEndpointdates(BeginDate, EndDate)
+    PrcntlArray = np.empty([YYYYMMDD_Of_PrcntlArray.shape[0], RefArray.shape[1]])
+    PrcntlArray[:] = np.NaN
+    if ( BeginYYYYMMDD in list(YYYYMMDD_Of_RefArray) ):
+      BeginIdx = list(YYYYMMDD_Of_RefArray).index(10000*BeginDateVecList[0] + 100*BeginDateVecList[1] + BeginDateVecList[2])
+      PrcntlArray[ : RefArray.shape[0]-BeginIdx ] = RefArray[ BeginIdx : ]
+    if ( EndYYYYMMDD in list(YYYYMMDD_Of_RefArray) ):
+      EndIdx = list(YYYYMMDD_Of_RefArray).index(10000*EndDateVecList[0] + 100*EndDateVecList[1] + EndDateVecList[2])
+      PrcntlArray[ PrcntlArray.shape[0]-(EndIdx+1) : ] = RefArray[ : EndIdx+1 ]
+  #end of if ( (BeginYYYYMMDD in list(YYYYMMDD_Of_RefArray) and...
+  return YYYYMMDD_Of_PrcntlArray, PrcntlArray
+#end of def TimeSlice_YYYYMMDDAndRefArrays(YYYYMMDD_Of_RefArray, RefArray, BeginDateVecList, EndDateVecList)
+
+def ReAssembleArraysFromMonthlyList(YYYYMMDD_Of_PrcntlArray, PrcntlArray, MonthlyList_YYYYMMDD_Of_PrcntlArray, MonthlyList_PrcntlArray):
+  YYYYMMDD_Of_PrcntlArray_1d = np.squeeze(YYYYMMDD_Of_PrcntlArray, axis = 1)
+  for WhichMonth in range(1,12+1):
+    YYYYMMDD_Of_PrcntlArray_ThisMonth_1d = np.squeeze(MonthlyList_YYYYMMDD_Of_PrcntlArray[WhichMonth-1], axis = 1)
+    all_sorted = np.argsort(YYYYMMDD_Of_PrcntlArray_1d)
+    ThisMonth_pos = np.searchsorted(YYYYMMDD_Of_PrcntlArray_1d[all_sorted], YYYYMMDD_Of_PrcntlArray_ThisMonth_1d)
+    indices = all_sorted[ThisMonth_pos]
+    PrcntlArray[indices, :] = MonthlyList_PrcntlArray[WhichMonth-1]
+  #end of for WhichMonth in range(1,12+1)
+  return PrcntlArray
+#end of def ReAssembleArraysFromMonthlyList(YYYYMMDD_Of_PrcntlArray, PrcntlArray, MonthlyList_YYYYMMDD_Of_PrcntlArray, MonthlyList_PrcntlArray)
+
+def PrepTrainPortion_ClimDivs_MonthlyPercBased(YYYYMMDD_Of_RefArray, RefArray, Training_BeginDateVecList, Training_EndDateVecList, MonthlyList_RefArray):
+
+  YYYYMMDD_Of_PrcntlArray, PrcntlArray = TimeSlice_YYYYMMDDAndRefArrays(YYYYMMDD_Of_RefArray, RefArray, Training_BeginDateVecList, Training_EndDateVecList)
+
+  MonthlyList_YYYYMMDD_Of_PrcntlArray, MonthlyList_PrcntlArray = CreateMonthlyLists_YYYYMMDDAndArray(YYYYMMDD_Of_PrcntlArray, PrcntlArray)
+
+  MonthlyList_PrcntlArray = LoopPrcntlCalcOverMonthsNSpatialUnits(MonthlyList_RefArray, MonthlyList_PrcntlArray)
+
+  PrcntlArray = ReAssembleArraysFromMonthlyList(YYYYMMDD_Of_PrcntlArray, PrcntlArray, MonthlyList_YYYYMMDD_Of_PrcntlArray, MonthlyList_PrcntlArray)
+
+  return YYYYMMDD_Of_PrcntlArray, PrcntlArray
