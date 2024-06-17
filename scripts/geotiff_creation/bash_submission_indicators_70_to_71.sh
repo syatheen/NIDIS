1b15fa3acd2d6c729c969887bca8b0a4dcfbe9ce
jordancaraballo <jordan.a.caraballo-vega@nasa.gov>
2024-06-17 11:08:11 -0400

    Updates to geotiff creation

--- scripts/geotiff_creation/bash_submission_indicators_70_to_71.sh
+++ scripts/geotiff_creation/bash_submission_indicators_70_to_71.sh
@@ -2,24 +2,24 @@
 
 enidis
 
-python CreateTifs.py 2000 -9999 >& CreateTifs.2000.out
-python CreateTifs.py 2001 -9999 >& CreateTifs.2001.out
-python CreateTifs.py 2002 -9999 >& CreateTifs.2002.out
-python CreateTifs.py 2003 -9999 >& CreateTifs.2003.out
-python CreateTifs.py 2004 -9999 >& CreateTifs.2004.out
-python CreateTifs.py 2005 -9999 >& CreateTifs.2005.out
-python CreateTifs.py 2006 -9999 >& CreateTifs.2006.out
-python CreateTifs.py 2007 -9999 >& CreateTifs.2007.out
-python CreateTifs.py 2008 -9999 >& CreateTifs.2008.out
-python CreateTifs.py 2009 -9999 >& CreateTifs.2009.out
-python CreateTifs.py 2010 -9999 >& CreateTifs.2010.out
-python CreateTifs.py 2011 -9999 >& CreateTifs.2011.out
-python CreateTifs.py 2012 -9999 >& CreateTifs.2012.out
-python CreateTifs.py 2013 -9999 >& CreateTifs.2013.out
-python CreateTifs.py 2014 -9999 >& CreateTifs.2014.out
-python CreateTifs.py 2015 -9999 >& CreateTifs.2015.out
-python CreateTifs.py 2016 -9999 >& CreateTifs.2016.out
-python CreateTifs.py 2017 -9999 >& CreateTifs.2017.out
-python CreateTifs.py 2018 -9999 >& CreateTifs.2018.out
-python CreateTifs.py 2019 -9999 >& CreateTifs.2019.out
-python CreateTifs.py 2020 151 >& CreateTifs.2020TillJDay151.out
+python $NOBACKUP/development/nidis/nidis/model/nclimgrid/geotiff_creation/CreateTifs_ESI_Indicators_70_to_71.py 2000 -9999 >& CreateTifs.2000.out
+python $NOBACKUP/development/nidis/nidis/model/nclimgrid/geotiff_creation/CreateTifs_ESI_Indicators_70_to_71.py 2001 -9999 >& CreateTifs.2001.out
+python $NOBACKUP/development/nidis/nidis/model/nclimgrid/geotiff_creation/CreateTifs_ESI_Indicators_70_to_71.py 2002 -9999 >& CreateTifs.2002.out
+python $NOBACKUP/development/nidis/nidis/model/nclimgrid/geotiff_creation/CreateTifs_ESI_Indicators_70_to_71.py 2003 -9999 >& CreateTifs.2003.out
+python $NOBACKUP/development/nidis/nidis/model/nclimgrid/geotiff_creation/CreateTifs_ESI_Indicators_70_to_71.py 2004 -9999 >& CreateTifs.2004.out
+python $NOBACKUP/development/nidis/nidis/model/nclimgrid/geotiff_creation/CreateTifs_ESI_Indicators_70_to_71.py 2005 -9999 >& CreateTifs.2005.out
+python $NOBACKUP/development/nidis/nidis/model/nclimgrid/geotiff_creation/CreateTifs_ESI_Indicators_70_to_71.py 2006 -9999 >& CreateTifs.2006.out
+python $NOBACKUP/development/nidis/nidis/model/nclimgrid/geotiff_creation/CreateTifs_ESI_Indicators_70_to_71.py 2007 -9999 >& CreateTifs.2007.out
+python $NOBACKUP/development/nidis/nidis/model/nclimgrid/geotiff_creation/CreateTifs_ESI_Indicators_70_to_71.py 2008 -9999 >& CreateTifs.2008.out
+python $NOBACKUP/development/nidis/nidis/model/nclimgrid/geotiff_creation/CreateTifs_ESI_Indicators_70_to_71.py 2009 -9999 >& CreateTifs.2009.out
+python $NOBACKUP/development/nidis/nidis/model/nclimgrid/geotiff_creation/CreateTifs_ESI_Indicators_70_to_71.py 2010 -9999 >& CreateTifs.2010.out
+python $NOBACKUP/development/nidis/nidis/model/nclimgrid/geotiff_creation/CreateTifs_ESI_Indicators_70_to_71.py 2011 -9999 >& CreateTifs.2011.out
+python $NOBACKUP/development/nidis/nidis/model/nclimgrid/geotiff_creation/CreateTifs_ESI_Indicators_70_to_71.py 2012 -9999 >& CreateTifs.2012.out
+python $NOBACKUP/development/nidis/nidis/model/nclimgrid/geotiff_creation/CreateTifs_ESI_Indicators_70_to_71.py 2013 -9999 >& CreateTifs.2013.out
+python $NOBACKUP/development/nidis/nidis/model/nclimgrid/geotiff_creation/CreateTifs_ESI_Indicators_70_to_71.py 2014 -9999 >& CreateTifs.2014.out
+python $NOBACKUP/development/nidis/nidis/model/nclimgrid/geotiff_creation/CreateTifs_ESI_Indicators_70_to_71.py 2015 -9999 >& CreateTifs.2015.out
+python $NOBACKUP/development/nidis/nidis/model/nclimgrid/geotiff_creation/CreateTifs_ESI_Indicators_70_to_71.py 2016 -9999 >& CreateTifs.2016.out
+python $NOBACKUP/development/nidis/nidis/model/nclimgrid/geotiff_creation/CreateTifs_ESI_Indicators_70_to_71.py 2017 -9999 >& CreateTifs.2017.out
+python $NOBACKUP/development/nidis/nidis/model/nclimgrid/geotiff_creation/CreateTifs_ESI_Indicators_70_to_71.py 2018 -9999 >& CreateTifs.2018.out
+python $NOBACKUP/development/nidis/nidis/model/nclimgrid/geotiff_creation/CreateTifs_ESI_Indicators_70_to_71.py 2019 -9999 >& CreateTifs.2019.out
+python $NOBACKUP/development/nidis/nidis/model/nclimgrid/geotiff_creation/CreateTifs_ESI_Indicators_70_to_71.py 2020 151 >& CreateTifs.2020TillJDay151.out
