Corrected Jordan's nClimGrid percentile calculation code for Palmer index indicators to overall percentile calculation instead of percentile calculation, as was done for nClimDiv resolution.

nidis/model/nclimgrid/percentile_creation/PrepTrainNEval_ClimGrid_Indicators_3_to_4.py
@@ -10,7 +10,7 @@ PHDI_RefFileName = '/discover/nobackup/projects/nca/jacaraba/NIDIS_Data/Indicato
 #USDM_InfoFileName = '/discover/nobackup/syatheen/NIDIS/Data/usdm_shapefiles/private/InfoArrWeekly_20000104To20200428.npz'
 #USDM_InfoFileName = '/att/nobackup/syatheen/Data/ML_Testcases/Drought_USDM/usdm_shapefiles/private/InfoArrWeekly_20000104To20200428.npz'
 
-TrainDataFilename = '/discover/nobackup/projects/nca/jacaraba/NIDIS_Data/Indicators_1_to_4/percentile_output/PreppedTrainNEvalNpzs/indicators_3_to_4/SingleUnified_'+str(Training_BeginDateVecList[0])+format(Training_BeginDateVecList[1],'02')+format(Training_BeginDateVecList[2],'02')+'To'+str(Training_EndDateVecList[0])+format(Training_EndDateVecList[1],'02')+format(Training_EndDateVecList[2],'02')+'.npz'
+TrainDataFilename = '/discover/nobackup/projects/nca/syatheen/NIDIS_Data/Indicators_1_to_4/percentile_output/PreppedTrainNEvalNpzs/indicators_3_to_4/SingleUnified_Corr2OverallPerc_'+str(Training_BeginDateVecList[0])+format(Training_BeginDateVecList[1],'02')+format(Training_BeginDateVecList[2],'02')+'To'+str(Training_EndDateVecList[0])+format(Training_EndDateVecList[1],'02')+format(Training_EndDateVecList[2],'02')+'.npz'
 
 # END code arguments / editable section
 
@@ -70,30 +70,6 @@ PHDI_RefArray = PHDI_RefObject['PHDI_RefArray']
 #CPCsoilmoist_RefArray = CPCsoilmoist_RefObject['CPCsoilmoist_RefArray']
 
 #USDM_InfoArray = USDM_InfoObject['InfoArray']
-
-def MonthlyList_YYYYMMDDAndArray(YYYYMMDD_Of_Array, ThisArray):
-  MM_Of_Array = (YYYYMMDD_Of_Array % 10000) // 100
-  MonthlyList_YYYYMMDD_Of_Array = []
-  MonthlyList_Array = []
-  for WhichMonth in range(1,12+1):
-    Idxs = np.where(MM_Of_Array == WhichMonth)
-    YYYYMMDD_Of_Array_ThisMonth = YYYYMMDD_Of_Array[Idxs[0]]
-    ThisArray_ThisMonth = ThisArray[Idxs[0]]
-    MonthlyList_YYYYMMDD_Of_Array.append(YYYYMMDD_Of_Array_ThisMonth)
-    MonthlyList_Array.append(ThisArray_ThisMonth)
-  #end of for WhichMonth in range(1,12+1):
-  return MonthlyList_YYYYMMDD_Of_Array, MonthlyList_Array
-#end of def MonthlyList_YYYYMMDDAndArray(YYYYMMDD_Of_Array, ThisArray):
-
-MonthlyList_PMDI_YYYYMMDD_Of_RefArray, MonthlyList_PMDI_RefArray = MonthlyList_YYYYMMDDAndArray(PMDI_YYYYMMDD_Of_RefArray, PMDI_RefArray)
-MonthlyList_PHDI_YYYYMMDD_Of_RefArray, MonthlyList_PHDI_RefArray = MonthlyList_YYYYMMDDAndArray(PHDI_YYYYMMDD_Of_RefArray, PHDI_RefArray)
-#MonthlyList_Pcpn_YYYYMMDD_Of_RefArray, MonthlyList_Pcpn1month_RefArray = MonthlyList_YYYYMMDDAndArray(Pcpn_YYYYMMDD_Of_RefArray, Pcpn1month_RefArray)
-#MonthlyList_Pcpn_YYYYMMDD_Of_RefArray, MonthlyList_Pcpn3month_RefArray = MonthlyList_YYYYMMDDAndArray(Pcpn_YYYYMMDD_Of_RefArray, Pcpn3month_RefArray)
-#MonthlyList_Pcpn_YYYYMMDD_Of_RefArray, MonthlyList_Pcpn6month_RefArray = MonthlyList_YYYYMMDDAndArray(Pcpn_YYYYMMDD_Of_RefArray, Pcpn6month_RefArray)
-#MonthlyList_Pcpn_YYYYMMDD_Of_RefArray, MonthlyList_Pcpn12month_RefArray = MonthlyList_YYYYMMDDAndArray(Pcpn_YYYYMMDD_Of_RefArray, Pcpn12month_RefArray)
-#MonthlyList_Pcpn_YYYYMMDD_Of_RefArray, MonthlyList_Pcpn24month_RefArray = MonthlyList_YYYYMMDDAndArray(Pcpn_YYYYMMDD_Of_RefArray, Pcpn24month_RefArray)
-#MonthlyList_Pcpn_YYYYMMDD_Of_RefArray, MonthlyList_Pcpn60month_RefArray = MonthlyList_YYYYMMDDAndArray(Pcpn_YYYYMMDD_Of_RefArray, Pcpn60month_RefArray)
-#MonthlyList_CPCsoilmoist_YYYYMMDD_Of_RefArray, MonthlyList_CPCsoilmoist_RefArray = MonthlyList_YYYYMMDDAndArray(CPCsoilmoist_YYYYMMDD_Of_RefArray, CPCsoilmoist_RefArray)
 
 def CreateYYYYMMDD_Of_Array(BeginDate, EndDate):
   TotalNumDaysDiff = abs(EndDate-BeginDate).days
@@ -159,28 +135,6 @@ def LoopPercentileCalcOverSpatialUnits(RefArray, PrcntlArray):
   return PrcntlArray
 #end of def LoopPercentileCalcOverSpatialUnits(RefArray, PrcntlArray):
 
-def LoopPrcntlCalcOverMonthsNSpatialUnits(MonthlyList_RefArray, MonthlyList_PrcntlArray):
-  for WhichMonth in range(1,12+1):
-    RefArray = MonthlyList_RefArray[WhichMonth-1]
-    PrcntlArray = MonthlyList_PrcntlArray[WhichMonth-1]
-    PrcntlArray = LoopPercentileCalcOverSpatialUnits(RefArray, PrcntlArray)
-    MonthlyList_PrcntlArray[WhichMonth-1] = PrcntlArray
-  #end of for WhichMonth in range(1,12+1):
-  return MonthlyList_PrcntlArray
-#end of def LoopPrcntlCalcOverMonthsNSpatialUnits(MonthlyList_RefArray, MonthlyList_PrcntlArray):
-
-def ReAssembleArraysFromMonthlyList(YYYYMMDD_Of_PrcntlArray, PrcntlArray, MonthlyList_YYYYMMDD_Of_PrcntlArray, MonthlyList_PrcntlArray):
-  YYYYMMDD_Of_PrcntlArray_1d = np.squeeze(YYYYMMDD_Of_PrcntlArray, axis = 1) 
-  for WhichMonth in range(1,12+1):
-    YYYYMMDD_Of_PrcntlArray_ThisMonth_1d = np.squeeze(MonthlyList_YYYYMMDD_Of_PrcntlArray[WhichMonth-1], axis = 1) 
-    all_sorted = np.argsort(YYYYMMDD_Of_PrcntlArray_1d) 
-    ThisMonth_pos = np.searchsorted(YYYYMMDD_Of_PrcntlArray_1d[all_sorted], YYYYMMDD_Of_PrcntlArray_ThisMonth_1d) 
-    indices = all_sorted[ThisMonth_pos]
-    PrcntlArray[indices, :] = MonthlyList_PrcntlArray[WhichMonth-1] 
-  #end of for WhichMonth in range(1,12+1):
-  return PrcntlArray
-#end of def ReAssembleArraysFromMonthlyList(YYYYMMDD_Of_PrcntlArray, PrcntlArray, MonthlyList_YYYYMMDD_Of_PrcntlArray, MonthlyList_PrcntlArray):
-
 def PrintInfoAboutArray(ThisArray, ThisArray_Str):
   print(ThisArray_Str,".shape is ",ThisArray.shape)
   print(ThisArray_Str,".dtype is ",ThisArray.dtype)
@@ -194,45 +148,9 @@ def PrintInfoAboutArray(ThisArray, ThisArray_Str):
 
 PMDI_YYYYMMDD_Of_PrcntlArray, PMDI_PrcntlArray = TimeSlice_YYYYMMDDAndRefArray(PMDI_YYYYMMDD_Of_RefArray, PMDI_RefArray, Training_BeginDateVecList, Training_EndDateVecList)
 PHDI_YYYYMMDD_Of_PrcntlArray, PHDI_PrcntlArray = TimeSlice_YYYYMMDDAndRefArray(PHDI_YYYYMMDD_Of_RefArray, PHDI_RefArray, Training_BeginDateVecList, Training_EndDateVecList)
-#Pcpn_YYYYMMDD_Of_PrcntlArray, Pcpn1month_PrcntlArray = TimeSlice_YYYYMMDDAndRefArray(Pcpn_YYYYMMDD_Of_RefArray, Pcpn1month_RefArray, Training_BeginDateVecList, Training_EndDateVecList)
-#Pcpn_YYYYMMDD_Of_PrcntlArray, Pcpn3month_PrcntlArray = TimeSlice_YYYYMMDDAndRefArray(Pcpn_YYYYMMDD_Of_RefArray, Pcpn3month_RefArray, Training_BeginDateVecList, Training_EndDateVecList)
-#Pcpn_YYYYMMDD_Of_PrcntlArray, Pcpn6month_PrcntlArray = TimeSlice_YYYYMMDDAndRefArray(Pcpn_YYYYMMDD_Of_RefArray, Pcpn6month_RefArray, Training_BeginDateVecList, Training_EndDateVecList)
-#Pcpn_YYYYMMDD_Of_PrcntlArray, Pcpn12month_PrcntlArray = TimeSlice_YYYYMMDDAndRefArray(Pcpn_YYYYMMDD_Of_RefArray, Pcpn12month_RefArray, Training_BeginDateVecList, Training_EndDateVecList)
-#Pcpn_YYYYMMDD_Of_PrcntlArray, Pcpn24month_PrcntlArray = TimeSlice_YYYYMMDDAndRefArray(Pcpn_YYYYMMDD_Of_RefArray, Pcpn24month_RefArray, Training_BeginDateVecList, Training_EndDateVecList)
-#Pcpn_YYYYMMDD_Of_PrcntlArray, Pcpn60month_PrcntlArray = TimeSlice_YYYYMMDDAndRefArray(Pcpn_YYYYMMDD_Of_RefArray, Pcpn60month_RefArray, Training_BeginDateVecList, Training_EndDateVecList)
-#CPCsoilmoist_YYYYMMDD_Of_PrcntlArray, CPCsoilmoist_PrcntlArray = TimeSlice_YYYYMMDDAndRefArray(CPCsoilmoist_YYYYMMDD_Of_RefArray, CPCsoilmoist_RefArray, Training_BeginDateVecList, Training_EndDateVecList)
 
-#USDM_YYYYMMDD_Of_TimeSlicedArray, USDM_TimeSlicedArray = TimeSlice_YYYYMMDDAndRefArray(USDM_YYYYMMDD_Of_InfoArray, USDM_InfoArray, Training_BeginDateVecList, Training_EndDateVecList)
-
-MonthlyList_PMDI_YYYYMMDD_Of_PrcntlArray, MonthlyList_PMDI_PrcntlArray = MonthlyList_YYYYMMDDAndArray(PMDI_YYYYMMDD_Of_PrcntlArray, PMDI_PrcntlArray)
-MonthlyList_PHDI_YYYYMMDD_Of_PrcntlArray, MonthlyList_PHDI_PrcntlArray = MonthlyList_YYYYMMDDAndArray(PHDI_YYYYMMDD_Of_PrcntlArray, PHDI_PrcntlArray)
-#MonthlyList_Pcpn_YYYYMMDD_Of_PrcntlArray, MonthlyList_Pcpn1month_PrcntlArray = MonthlyList_YYYYMMDDAndArray(Pcpn_YYYYMMDD_Of_PrcntlArray, Pcpn1month_PrcntlArray)
-#MonthlyList_Pcpn_YYYYMMDD_Of_PrcntlArray, MonthlyList_Pcpn3month_PrcntlArray = MonthlyList_YYYYMMDDAndArray(Pcpn_YYYYMMDD_Of_PrcntlArray, Pcpn3month_PrcntlArray)
-#MonthlyList_Pcpn_YYYYMMDD_Of_PrcntlArray, MonthlyList_Pcpn6month_PrcntlArray = MonthlyList_YYYYMMDDAndArray(Pcpn_YYYYMMDD_Of_PrcntlArray, Pcpn6month_PrcntlArray)
-#MonthlyList_Pcpn_YYYYMMDD_Of_PrcntlArray, MonthlyList_Pcpn12month_PrcntlArray = MonthlyList_YYYYMMDDAndArray(Pcpn_YYYYMMDD_Of_PrcntlArray, Pcpn12month_PrcntlArray)
-#MonthlyList_Pcpn_YYYYMMDD_Of_PrcntlArray, MonthlyList_Pcpn24month_PrcntlArray = MonthlyList_YYYYMMDDAndArray(Pcpn_YYYYMMDD_Of_PrcntlArray, Pcpn24month_PrcntlArray)
-#MonthlyList_Pcpn_YYYYMMDD_Of_PrcntlArray, MonthlyList_Pcpn60month_PrcntlArray = MonthlyList_YYYYMMDDAndArray(Pcpn_YYYYMMDD_Of_PrcntlArray, Pcpn60month_PrcntlArray)
-#MonthlyList_CPCsoilmoist_YYYYMMDD_Of_PrcntlArray, MonthlyList_CPCsoilmoist_PrcntlArray = MonthlyList_YYYYMMDDAndArray(CPCsoilmoist_YYYYMMDD_Of_PrcntlArray, CPCsoilmoist_PrcntlArray)
-
-MonthlyList_PMDI_PrcntlArray = LoopPrcntlCalcOverMonthsNSpatialUnits(MonthlyList_PMDI_RefArray, MonthlyList_PMDI_PrcntlArray)
-MonthlyList_PHDI_PrcntlArray = LoopPrcntlCalcOverMonthsNSpatialUnits(MonthlyList_PHDI_RefArray, MonthlyList_PHDI_PrcntlArray)
-#MonthlyList_Pcpn1month_PrcntlArray = LoopPrcntlCalcOverMonthsNSpatialUnits(MonthlyList_Pcpn1month_RefArray, MonthlyList_Pcpn1month_PrcntlArray)
-#MonthlyList_Pcpn3month_PrcntlArray = LoopPrcntlCalcOverMonthsNSpatialUnits(MonthlyList_Pcpn3month_RefArray, MonthlyList_Pcpn3month_PrcntlArray)
-#MonthlyList_Pcpn6month_PrcntlArray = LoopPrcntlCalcOverMonthsNSpatialUnits(MonthlyList_Pcpn6month_RefArray, MonthlyList_Pcpn6month_PrcntlArray)
-#MonthlyList_Pcpn12month_PrcntlArray = LoopPrcntlCalcOverMonthsNSpatialUnits(MonthlyList_Pcpn12month_RefArray, MonthlyList_Pcpn12month_PrcntlArray)
-#MonthlyList_Pcpn24month_PrcntlArray = LoopPrcntlCalcOverMonthsNSpatialUnits(MonthlyList_Pcpn24month_RefArray, MonthlyList_Pcpn24month_PrcntlArray)
-#MonthlyList_Pcpn60month_PrcntlArray = LoopPrcntlCalcOverMonthsNSpatialUnits(MonthlyList_Pcpn60month_RefArray, MonthlyList_Pcpn60month_PrcntlArray)
-#MonthlyList_CPCsoilmoist_PrcntlArray = LoopPrcntlCalcOverMonthsNSpatialUnits(MonthlyList_CPCsoilmoist_RefArray, MonthlyList_CPCsoilmoist_PrcntlArray)
-
-PMDI_PrcntlArray = ReAssembleArraysFromMonthlyList(PMDI_YYYYMMDD_Of_PrcntlArray, PMDI_PrcntlArray, MonthlyList_PMDI_YYYYMMDD_Of_PrcntlArray, MonthlyList_PMDI_PrcntlArray)
-PHDI_PrcntlArray = ReAssembleArraysFromMonthlyList(PHDI_YYYYMMDD_Of_PrcntlArray, PHDI_PrcntlArray, MonthlyList_PHDI_YYYYMMDD_Of_PrcntlArray, MonthlyList_PHDI_PrcntlArray)
-#Pcpn1month_PrcntlArray = ReAssembleArraysFromMonthlyList(Pcpn_YYYYMMDD_Of_PrcntlArray, Pcpn1month_PrcntlArray, MonthlyList_Pcpn_YYYYMMDD_Of_PrcntlArray, MonthlyList_Pcpn1month_PrcntlArray)
-#Pcpn3month_PrcntlArray = ReAssembleArraysFromMonthlyList(Pcpn_YYYYMMDD_Of_PrcntlArray, Pcpn3month_PrcntlArray, MonthlyList_Pcpn_YYYYMMDD_Of_PrcntlArray, MonthlyList_Pcpn3month_PrcntlArray)
-#Pcpn6month_PrcntlArray = ReAssembleArraysFromMonthlyList(Pcpn_YYYYMMDD_Of_PrcntlArray, Pcpn6month_PrcntlArray, MonthlyList_Pcpn_YYYYMMDD_Of_PrcntlArray, MonthlyList_Pcpn6month_PrcntlArray)
-#Pcpn12month_PrcntlArray = ReAssembleArraysFromMonthlyList(Pcpn_YYYYMMDD_Of_PrcntlArray, Pcpn12month_PrcntlArray, MonthlyList_Pcpn_YYYYMMDD_Of_PrcntlArray, MonthlyList_Pcpn12month_PrcntlArray)
-#Pcpn24month_PrcntlArray = ReAssembleArraysFromMonthlyList(Pcpn_YYYYMMDD_Of_PrcntlArray, Pcpn24month_PrcntlArray, MonthlyList_Pcpn_YYYYMMDD_Of_PrcntlArray, MonthlyList_Pcpn24month_PrcntlArray)
-#Pcpn60month_PrcntlArray = ReAssembleArraysFromMonthlyList(Pcpn_YYYYMMDD_Of_PrcntlArray, Pcpn60month_PrcntlArray, MonthlyList_Pcpn_YYYYMMDD_Of_PrcntlArray, MonthlyList_Pcpn60month_PrcntlArray)
-#CPCsoilmoist_PrcntlArray = ReAssembleArraysFromMonthlyList(CPCsoilmoist_YYYYMMDD_Of_PrcntlArray, CPCsoilmoist_PrcntlArray, MonthlyList_CPCsoilmoist_YYYYMMDD_Of_PrcntlArray, MonthlyList_CPCsoilmoist_PrcntlArray)
+PMDI_PrcntlArray = LoopPercentileCalcOverSpatialUnits(PMDI_RefArray, PMDI_PrcntlArray)
+PHDI_PrcntlArray = LoopPercentileCalcOverSpatialUnits(PHDI_RefArray, PHDI_PrcntlArray)
 
 PrintInfoAboutArray(PMDI_PrcntlArray, 'PMDI_PrcntlArray')
 PrintInfoAboutArray(PHDI_PrcntlArray, 'PHDI_PrcntlArray')
@@ -249,7 +167,6 @@ PrintInfoAboutArray(PHDI_PrcntlArray, 'PHDI_PrcntlArray')
 np.savez_compressed(TrainDataFilename, YYYYMMDD_Of_Array = PMDI_YYYYMMDD_Of_PrcntlArray, PMDI_PrcntlArray = PMDI_PrcntlArray, PHDI_PrcntlArray = PHDI_PrcntlArray)#, Pcpn1month_PrcntlArray = Pcpn1month_PrcntlArray, Pcpn3month_PrcntlArray = Pcpn3month_PrcntlArray, Pcpn6month_PrcntlArray = Pcpn6month_PrcntlArray, Pcpn12month_PrcntlArray = Pcpn12month_PrcntlArray, Pcpn24month_PrcntlArray = Pcpn24month_PrcntlArray, Pcpn60month_PrcntlArray = Pcpn60month_PrcntlArray, CPCsoilmoist_PrcntlArray = CPCsoilmoist_PrcntlArray)#, USDM_TimeSlicedArray = USDM_TimeSlicedArray) 
 
 #END section for training
-
 
 eeend_Overall = datetime.now()
 eeelapsed_Overall = eeend_Overall - ssstart_Overall