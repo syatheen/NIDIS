fb98d46e9df7569e44cc3af475f9067f39926fb0
Soni Yatheendradas <soni.yatheendradas-1@nasa.gov>
2024-12-20 17:01:31 -0500

    Clipped Jordan's nClimGrid weekly resolution code for Z- and Palmer index indicators to that for only Palmer index indicators; the correct Z index indicators' code file already exists.

--- nidis/model/nclimgrid/weekly_resolution/PrepRefArraysFromInfoArrays_ClimGrid1D_Indicators_3_to_4.py
+++ nidis/model/nclimgrid/weekly_resolution/PrepRefArraysFromInfoArrays_ClimGrid1D_Indicators_3_to_4.py
@@ -5,43 +5,27 @@ import numpy as np
 from calendar import monthrange
 import time
 
-ZIndex_InfoFilename = '/discover/nobackup/projects/nca/jacaraba/NIDIS_Data/Indicators_1_to_4/RefArr_1MonthAccumzndx_193201To202001.npz' 
-ZIndex60month_InfoFilename = '/discover/nobackup/projects/nca/jacaraba/NIDIS_Data/Indicators_1_to_4/RefArr_60MonthAccumzndx_193201To202001.npz' 
 PMDI_InfoFilename = '/discover/nobackup/projects/nca/jacaraba/NIDIS_Data/Indicators_1_to_4/pdiRefArray_20050101To20200104_ClmGrd1D.npz' 
 PHDI_InfoFilename = '/discover/nobackup/projects/nca/jacaraba/NIDIS_Data/Indicators_1_to_4/phdRefArray_20050604To20200104_ClmGrd1D.npz'
 
-ZIndex_Ref_BeginDateVecList = [1932, 1, 19]  # ZIndex beginning year, month, day of month, this is also a Tuesday
-ZIndex_Ref_EndDateVecList = [2019, 12, 31]  # ZIndex ending year, month, day of month, this is also a Tuesday
 PMDI_Ref_BeginDateVecList = [2005, 1, 4]  # PMDI beginning year, month, day of month, this is also a Tuesday
 PMDI_Ref_EndDateVecList = [2019, 12, 31]  # PMDI ending year, month, day of month, this is also a Tuesday
 PHDI_Ref_BeginDateVecList = [2005, 6, 7]  # PHDI beginning year, month, day of month, this is also a Tuesday
 PHDI_Ref_EndDateVecList = [2019, 12, 31]  # PHDI ending year, month, day of month, this is also a Tuesday
 
-
-ZIndex_RefFileName = '/discover/nobackup/projects/nca/jacaraba/NIDIS_Data/Indicators_1_to_4/weekly_resolution_output/ClimGrid_ZIndex_'+format(ZIndex_Ref_BeginDateVecList[0],'04')+format(ZIndex_Ref_BeginDateVecList[1],'02')+format(ZIndex_Ref_BeginDateVecList[2],'02')+'To'+format(ZIndex_Ref_EndDateVecList[0],'04')+format(ZIndex_Ref_EndDateVecList[1],'02')+format(ZIndex_Ref_EndDateVecList[2],'02')+'.npz'
-ZIndex60month_RefFileName = '/discover/nobackup/projects/nca/jacaraba/NIDIS_Data/Indicators_1_to_4/weekly_resolution_output/ClimGrid_ZIndex60month_'+format(ZIndex_Ref_BeginDateVecList[0],'04')+format(ZIndex_Ref_BeginDateVecList[1],'02')+format(ZIndex_Ref_BeginDateVecList[2],'02')+'To'+format(ZIndex_Ref_EndDateVecList[0],'04')+format(ZIndex_Ref_EndDateVecList[1],'02')+format(ZIndex_Ref_EndDateVecList[2],'02')+'.npz'
 PMDI_RefFileName = '/discover/nobackup/projects/nca/jacaraba/NIDIS_Data/Indicators_1_to_4/weekly_resolution_output/ClimGrid_PMDI_'+format(PMDI_Ref_BeginDateVecList[0],'04')+format(PMDI_Ref_BeginDateVecList[1],'02')+format(PMDI_Ref_BeginDateVecList[2],'02')+'To'+format(PMDI_Ref_EndDateVecList[0],'04')+format(PMDI_Ref_EndDateVecList[1],'02')+format(PMDI_Ref_EndDateVecList[2],'02')+'.npz'
 PHDI_RefFileName = '/discover/nobackup/projects/nca/jacaraba/NIDIS_Data/Indicators_1_to_4/weekly_resolution_output/ClimGrid_PHDI_'+format(PHDI_Ref_BeginDateVecList[0],'04')+format(PHDI_Ref_BeginDateVecList[1],'02')+format(PHDI_Ref_BeginDateVecList[2],'02')+'To'+format(PHDI_Ref_EndDateVecList[0],'04')+format(PHDI_Ref_EndDateVecList[1],'02')+format(PHDI_Ref_EndDateVecList[2],'02')+'.npz'
 
 # END code arguments / editable section
 
-
 start_time = time.time()
 
-ZIndex_Ref_BeginDate = date(ZIndex_Ref_BeginDateVecList[0], ZIndex_Ref_BeginDateVecList[1], ZIndex_Ref_BeginDateVecList[2])
-ZIndex_Ref_EndDate = date(ZIndex_Ref_EndDateVecList[0], ZIndex_Ref_EndDateVecList[1], ZIndex_Ref_EndDateVecList[2])
 PMDI_Ref_BeginDate = date(PMDI_Ref_BeginDateVecList[0], PMDI_Ref_BeginDateVecList[1], PMDI_Ref_BeginDateVecList[2])
 PMDI_Ref_EndDate = date(PMDI_Ref_EndDateVecList[0], PMDI_Ref_EndDateVecList[1], PMDI_Ref_EndDateVecList[2])
 PHDI_Ref_BeginDate = date(PHDI_Ref_BeginDateVecList[0], PHDI_Ref_BeginDateVecList[1], PHDI_Ref_BeginDateVecList[2])
 PHDI_Ref_EndDate = date(PHDI_Ref_EndDateVecList[0], PHDI_Ref_EndDateVecList[1], PHDI_Ref_EndDateVecList[2])
 
 #BEGIN check whether the beginning and ending days are indeed Tuesdays
-if ZIndex_Ref_BeginDate.weekday() != 1:  # 0 for Monday, 1 for Tuesday
-  print('Beginning Date Vector for ZIndex_Ref needs to be a Tuesday!!')
-  sys.exit(0)
-if ZIndex_Ref_EndDate.weekday() != 1:  # 0 for Monday, 1 for Tuesday
-  print('Ending Date Vector for ZIndex_Ref needs to be a Tuesday!!')
-  sys.exit(0)
 if PMDI_Ref_BeginDate.weekday() != 1:  # 0 for Monday, 1 for Tuesday
   print('Beginning Date Vector for PMDI_Ref needs to be a Tuesday!!')
   sys.exit(0)
@@ -57,9 +41,6 @@ if PHDI_Ref_EndDate.weekday() != 1:  # 0 for Monday, 1 for Tuesday
 
 #END check whether the beginning and ending days are indeed Tuesdays
 
-if ZIndex_Ref_BeginDate > ZIndex_Ref_EndDate:
-  print('ZIndex_Ref_BeginDate should not be later than ZIndex_Ref_EndDate!!!')
-  sys.exit(0)
 if PMDI_Ref_BeginDate > PMDI_Ref_EndDate:
   print('PMDI_Ref_BeginDate should not be later than PMDI_Ref_EndDate!!!')
   sys.exit(0)
@@ -70,16 +51,10 @@ if PHDI_Ref_BeginDate > PHDI_Ref_EndDate:
 
 #BEGIN section for time-interpolating info arrays to desired temporal resolution
 
-ZIndex_Info = np.load(ZIndex_InfoFilename)
-ZIndex60month_Info = np.load(ZIndex60month_InfoFilename)
 PMDI_Info = np.load(PMDI_InfoFilename)
 PHDI_Info = np.load(PHDI_InfoFilename)
 #USDM_Info = np.load(USDM_InfoFilename)
 
-ZIndex_YYYYMM_Of_InfoArray  = ZIndex_Info['YYYYMM_Of_RefArrayForPrcntl']
-ZIndex_InfoArray  = ZIndex_Info['RefArrayForPrcntl']
-ZIndex60month_YYYYMM_Of_InfoArray  = ZIndex60month_Info['YYYYMM_Of_RefArrayForPrcntl']
-ZIndex60month_InfoArray  = ZIndex60month_Info['RefArrayForPrcntl']
 PMDI_YYYYMMDD_Of_InfoArray  = PMDI_Info['YYYYMMDD_Of_RefArrayForPrcntl']
 PMDI_YYYYMMDD_Of_InfoArray = np.expand_dims(PMDI_YYYYMMDD_Of_InfoArray, axis = 1)
 PMDI_InfoArray  = PMDI_Info['Palmer_RefArrayForPrcntl']
@@ -117,8 +92,6 @@ def GetRealDatesListOfInfoArray(YYYYMMEtc_Of_InfoArray, NumDateElements, VariabN
   return RealDatesList_Of_InfoArray
 #end of def GetRealDatesListOfInfoArray(YYYYMMEtc_Of_InfoArray, NumDateElements, VariabNameForErrStr, WhereDayForNumDateElements2):
 
-ZIndex_RealDatesList_Of_InfoArray = GetRealDatesListOfInfoArray(ZIndex_YYYYMM_Of_InfoArray, 2, 'ZIndex', 'mid')
-ZIndex60month_RealDatesList_Of_InfoArray = GetRealDatesListOfInfoArray(ZIndex60month_YYYYMM_Of_InfoArray, 2, 'ZIndex60month', 'mid')
 PMDI_RealDatesList_Of_InfoArray = GetRealDatesListOfInfoArray(PMDI_YYYYMMDD_Of_InfoArray, 3, 'PMDI', np.NaN)
 PHDI_RealDatesList_Of_InfoArray = GetRealDatesListOfInfoArray(PHDI_YYYYMMDD_Of_InfoArray, 3, 'PHDI', np.NaN)
 # USDM_RealDatesList_Of_InfoArray = GetRealDatesListOfInfoArray(USDM_YYYYMMDD_Of_InfoArray, 3, 'USDM', np.NaN)
@@ -137,15 +110,10 @@ def GetTimeInfoOfRefArray(Ref_BeginDate, Ref_EndDate):
   YYYYMMDD_Of_RefArray = np.expand_dims(YYYYMMDD_Of_RefArray, axis=1)
   return RealDatesList_Of_RefArray, YYYYMMDD_Of_RefArray
 #end of def GetTimeInfoOfRefArray(Ref_BeginDate, Ref_EndDate):
-ZIndex_RealDatesList_Of_RefArray, ZIndex_YYYYMMDD_Of_RefArray  = GetTimeInfoOfRefArray(ZIndex_Ref_BeginDate, ZIndex_Ref_EndDate)
 PMDI_RealDatesList_Of_RefArray, PMDI_YYYYMMDD_Of_RefArray = GetTimeInfoOfRefArray(PMDI_Ref_BeginDate, PMDI_Ref_EndDate)
 PHDI_RealDatesList_Of_RefArray, PHDI_YYYYMMDD_Of_RefArray = GetTimeInfoOfRefArray(PHDI_Ref_BeginDate, PHDI_Ref_EndDate)
 #End calculating real dates list for reference arrays
 
-ZIndex_RefArray = np.empty([ ZIndex_YYYYMMDD_Of_RefArray.shape[0], ZIndex_InfoArray.shape[1]])
-ZIndex_RefArray[:] = np.NaN
-ZIndex60month_RefArray = np.empty([ ZIndex_YYYYMMDD_Of_RefArray.shape[0], ZIndex60month_InfoArray.shape[1]])
-ZIndex60month_RefArray[:] = np.NaN
 PMDI_RefArray = np.empty([ PMDI_YYYYMMDD_Of_RefArray.shape[0], PMDI_InfoArray.shape[1]])
 PMDI_RefArray[:] = np.NaN
 PHDI_RefArray = np.empty([ PHDI_YYYYMMDD_Of_RefArray.shape[0], PHDI_InfoArray.shape[1]])
@@ -189,8 +157,6 @@ def TimeInterpolateValues(SourceDatesList, DestinationDatesList, SourceArray, De
   return DestinationArray
 #end of def TimeInterpolateValues(SourceDatesList, DestinationDatesList, SourceArray, DestinationArray):
  
-ZIndex_RefArray = TimeInterpolateValues(ZIndex_RealDatesList_Of_InfoArray, ZIndex_RealDatesList_Of_RefArray, ZIndex_InfoArray, ZIndex_RefArray)
-ZIndex60month_RefArray = TimeInterpolateValues(ZIndex60month_RealDatesList_Of_InfoArray, ZIndex_RealDatesList_Of_RefArray, ZIndex60month_InfoArray, ZIndex60month_RefArray)
 PMDI_RefArray = TimeInterpolateValues(PMDI_RealDatesList_Of_InfoArray, PMDI_RealDatesList_Of_RefArray, PMDI_InfoArray, PMDI_RefArray)
 PHDI_RefArray = TimeInterpolateValues(PHDI_RealDatesList_Of_InfoArray, PHDI_RealDatesList_Of_RefArray, PHDI_InfoArray, PHDI_RefArray)
 
@@ -212,25 +178,8 @@ def TimeMeanValues(SourceDatesList, DestinationDatesList, SourceArray, Destinati
 
 print("--- %s seconds ---" % (time.time() - start_time))
 
-np.savez_compressed(ZIndex_RefFileName, ZIndex_RefArray = ZIndex_RefArray, ZIndex_YYYYMMDD_Of_RefArray = ZIndex_YYYYMMDD_Of_RefArray)
-np.savez_compressed(ZIndex60month_RefFileName, ZIndex60month_RefArray = ZIndex60month_RefArray, ZIndex_YYYYMMDD_Of_RefArray = ZIndex_YYYYMMDD_Of_RefArray)
 np.savez_compressed(PMDI_RefFileName, PMDI_RefArray = PMDI_RefArray, PMDI_YYYYMMDD_Of_RefArray = PMDI_YYYYMMDD_Of_RefArray)
 np.savez_compressed(PHDI_RefFileName, PHDI_RefArray = PHDI_RefArray, PHDI_YYYYMMDD_Of_RefArray = PHDI_YYYYMMDD_Of_RefArray)
-
-print("ZIndex_YYYYMMDD_Of_RefArray.shape is ",ZIndex_YYYYMMDD_Of_RefArray.shape)
-print("ZIndex_YYYYMMDD_Of_RefArray is ",ZIndex_YYYYMMDD_Of_RefArray)
-
-print("ZIndex_RefArray.shape is ",ZIndex_RefArray.shape)
-print("ZIndex_RefArray is ",ZIndex_RefArray)
-print("np.amin(np.isnan(ZIndex_RefArray).sum(axis=0)) is ",np.amin(np.isnan(ZIndex_RefArray).sum(axis=0)))
-print("np.amax(np.isnan(ZIndex_RefArray).sum(axis=0)) is ",np.amax(np.isnan(ZIndex_RefArray).sum(axis=0)))
-print('overall min is ',np.nanmin(ZIndex_RefArray),', overall max is ',np.nanmax(ZIndex_RefArray))
-
-print("ZIndex60month_RefArray.shape is ",ZIndex60month_RefArray.shape)
-print("ZIndex60month_RefArray is ",ZIndex60month_RefArray)
-print("np.amin(np.isnan(ZIndex60month_RefArray).sum(axis=0)) is ",np.amin(np.isnan(ZIndex60month_RefArray).sum(axis=0)))
-print("np.amax(np.isnan(ZIndex60month_RefArray).sum(axis=0)) is ",np.amax(np.isnan(ZIndex60month_RefArray).sum(axis=0)))
-print('overall min is ',np.nanmin(ZIndex60month_RefArray),', overall max is ',np.nanmax(ZIndex60month_RefArray))
 
 print("PMDI_RefArray.shape is ",PMDI_RefArray.shape)
 print("PMDI_RefArray is ",PMDI_RefArray)
