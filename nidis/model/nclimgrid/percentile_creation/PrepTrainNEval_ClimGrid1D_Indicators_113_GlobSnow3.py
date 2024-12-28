1900d386d23bc3be01b9a200bfadc3124c3395b6
Soni Yatheendradas <soni.yatheendradas-1@nasa.gov>
2024-12-28 13:06:31 -0500

    GlobSnow3 indicator nClimGrid codes for weekly resolution and percentile correction corrected because no zero-fill was done (former) and wrong percentile correction methodology was used.

--- nidis/model/nclimgrid/percentile_creation/PrepTrainNEval_ClimGrid1D_Indicators_113_GlobSnow3.py
+++ nidis/model/nclimgrid/percentile_creation/PrepTrainNEval_ClimGrid1D_Indicators_113_GlobSnow3.py
@@ -4,9 +4,9 @@
 Training_BeginDateVecList = [2006, 1, 3] # Beginning training year, month, day of month, this is also a Tuesday
 Training_EndDateVecList = [2019, 12, 31] # Ending training year, month, day of month, this is also a Tuesday
 
-CPCsoilmoist_RefFileName = '/discover/nobackup/projects/nca/jacaraba/NIDIS_Data/RefArrays/ClimGrid1D_GlobSnow3_19800101To20210831.npz'
+GlobSnow3_RefFileName = '/discover/nobackup/syatheen/Sujay/DeepLearning/ExampleTries/NIDIS/RefArrays/ClimGrid1D_GlobSnow3_19790109To20180529.npz'
 
-TrainDataFilename = '/discover/nobackup/projects/nca/jacaraba/NIDIS_Data/Indicator_113/percentile_creation/PreppedTrainNEvalNpzs/SingleUnified_'+str(Training_BeginDateVecList[0])+format(Training_BeginDateVecList[1],'02')+format(Training_BeginDateVecList[2],'02')+'To'+str(Training_EndDateVecList[0])+format(Training_EndDateVecList[1],'02')+format(Training_EndDateVecList[2],'02')+'.npz'
+TrainDataFilename = '/discover/nobackup/syatheen/Sujay/DeepLearning/ExampleTries/NIDIS/PreppedTrainNEvalNpzs/ClimGrid1D/SingleUnified_GlobSnow3_'+str(Training_BeginDateVecList[0])+format(Training_BeginDateVecList[1],'02')+format(Training_BeginDateVecList[2],'02')+'To'+str(Training_EndDateVecList[0])+format(Training_EndDateVecList[1],'02')+format(Training_EndDateVecList[2],'02')+'.npz'
 
 # END code arguments / editable section
 
@@ -35,31 +35,14 @@ if Training_BeginDate > Training_EndDate:
   print('Training_BeginDate should not be later than Training_EndDate!!!')
   sys.exit(0)
 
-CPCsoilmoist_RefObject = np.load(CPCsoilmoist_RefFileName)
-CPCsoilmoist_YYYYMMDD_Of_RefArray = CPCsoilmoist_RefObject['GlobSnow3_YYYYMMDD_Of_RefArray']
+GlobSnow3_RefObject = np.load(GlobSnow3_RefFileName)
 
-CPCsoilmoist_RefArray = CPCsoilmoist_RefObject['GlobSnow3_RefArray']
+GlobSnow3_YYYYMMDD_Of_RefArray = GlobSnow3_RefObject['GlobSnow3_YYYYMMDD_Of_RefArray']
 
-print(CPCsoilmoist_YYYYMMDD_Of_RefArray.shape)
-print(CPCsoilmoist_RefArray.shape)
+GlobSnow3_RefArray = GlobSnow3_RefObject['GlobSnow3_RefArray']
 
-def MonthlyList_YYYYMMDDAndArray(YYYYMMDD_Of_Array, ThisArray):
-  MM_Of_Array = (YYYYMMDD_Of_Array % 10000) // 100
-  MonthlyList_YYYYMMDD_Of_Array = []
-  MonthlyList_Array = []
-  for WhichMonth in range(1,12+1):
-    Idxs = np.where(MM_Of_Array == WhichMonth)
-    YYYYMMDD_Of_Array_ThisMonth = YYYYMMDD_Of_Array[Idxs[0]]
-    ThisArray_ThisMonth = ThisArray[Idxs[0]]
-    MonthlyList_YYYYMMDD_Of_Array.append(YYYYMMDD_Of_Array_ThisMonth)
-    MonthlyList_Array.append(ThisArray_ThisMonth)
-  #end of for WhichMonth in range(1,12+1):
-  return MonthlyList_YYYYMMDD_Of_Array, MonthlyList_Array
-#end of def MonthlyList_YYYYMMDDAndArray(YYYYMMDD_Of_Array, ThisArray):
-
-MonthlyList_CPCsoilmoist_YYYYMMDD_Of_RefArray, MonthlyList_CPCsoilmoist_RefArray = MonthlyList_YYYYMMDDAndArray(CPCsoilmoist_YYYYMMDD_Of_RefArray, CPCsoilmoist_RefArray)
-
-print("Done with MonthlyList_CPCsoilmoist_YYYYMMDD_Of_RefArray", type(MonthlyList_CPCsoilmoist_YYYYMMDD_Of_RefArray), type(MonthlyList_CPCsoilmoist_RefArray))
+#print(GlobSnow3_YYYYMMDD_Of_RefArray.shape)
+#print(GlobSnow3_RefArray.shape)
 
 def CreateYYYYMMDD_Of_Array(BeginDate, EndDate):
   TotalNumDaysDiff = abs(EndDate-BeginDate).days
@@ -140,52 +123,30 @@ def LoopPercentileCalcOverSpatialUnits(RefArray, PrcntlArray):
   return PrcntlArray
 #end of def LoopPercentileCalcOverSpatialUnits(RefArray, PrcntlArray):
 
-def LoopPrcntlCalcOverMonthsNSpatialUnits(MonthlyList_RefArray, MonthlyList_PrcntlArray):
-  for WhichMonth in range(1,12+1):
-    RefArray = MonthlyList_RefArray[WhichMonth-1]
-    PrcntlArray = MonthlyList_PrcntlArray[WhichMonth-1]
-    PrcntlArray = LoopPercentileCalcOverSpatialUnits(RefArray, PrcntlArray)
-    MonthlyList_PrcntlArray[WhichMonth-1] = PrcntlArray
-  #end of for WhichMonth in range(1,12+1):
-  return MonthlyList_PrcntlArray
-#end of def LoopPrcntlCalcOverMonthsNSpatialUnits(MonthlyList_RefArray, MonthlyList_PrcntlArray):
-
-def ReAssembleArraysFromMonthlyList(YYYYMMDD_Of_PrcntlArray, PrcntlArray, MonthlyList_YYYYMMDD_Of_PrcntlArray, MonthlyList_PrcntlArray):
-  YYYYMMDD_Of_PrcntlArray_1d = np.squeeze(YYYYMMDD_Of_PrcntlArray, axis = 1) 
-  for WhichMonth in range(1,12+1):
-    YYYYMMDD_Of_PrcntlArray_ThisMonth_1d = np.squeeze(MonthlyList_YYYYMMDD_Of_PrcntlArray[WhichMonth-1], axis = 1) 
-    all_sorted = np.argsort(YYYYMMDD_Of_PrcntlArray_1d) 
-    ThisMonth_pos = np.searchsorted(YYYYMMDD_Of_PrcntlArray_1d[all_sorted], YYYYMMDD_Of_PrcntlArray_ThisMonth_1d) 
-    indices = all_sorted[ThisMonth_pos]
-    PrcntlArray[indices, :] = MonthlyList_PrcntlArray[WhichMonth-1] 
-  #end of for WhichMonth in range(1,12+1):
-  return PrcntlArray
-#end of def ReAssembleArraysFromMonthlyList(YYYYMMDD_Of_PrcntlArray, PrcntlArray, MonthlyList_YYYYMMDD_Of_PrcntlArray, MonthlyList_PrcntlArray):
-
 def PrintInfoAboutArray(ThisArray, ThisArray_Str):
   print(ThisArray_Str,".shape is ",ThisArray.shape)
   print(ThisArray_Str,".dtype is ",ThisArray.dtype)
   print(ThisArray_Str," is ",ThisArray)
   print("np.amin(np.isnan(",ThisArray_Str,").sum(axis=0)) is ",np.amin(np.isnan(ThisArray).sum(axis=0)))
   print("np.amax(np.isnan(",ThisArray_Str,").sum(axis=0)) is ",np.amax(np.isnan(ThisArray).sum(axis=0)))
+  print("np.amin(np.isnan(",ThisArray_Str,").sum(axis=1)) is ",np.amin(np.isnan(ThisArray).sum(axis=1)))
+  print("np.amax(np.isnan(",ThisArray_Str,").sum(axis=1)) is ",np.amax(np.isnan(ThisArray).sum(axis=1)))
   print('overall min is ',np.nanmin(ThisArray),', overall max is ',np.nanmax(ThisArray))
 #end of def PrintInfoAboutArray(ThisArray, ThisArray_Str):
 
 #BEGIN section for training
 
-CPCsoilmoist_YYYYMMDD_Of_PrcntlArray, CPCsoilmoist_PrcntlArray = TimeSlice_YYYYMMDDAndRefArray(CPCsoilmoist_YYYYMMDD_Of_RefArray, CPCsoilmoist_RefArray, Training_BeginDateVecList, Training_EndDateVecList)
-print("After CPCsoilmoist_YYYYMMDD_Of_PrcntlArray CPCsoilmoist_PrcntlArray")
+GlobSnow3_YYYYMMDD_Of_PrcntlArray, GlobSnow3_PrcntlArray = TimeSlice_YYYYMMDDAndRefArray(GlobSnow3_YYYYMMDD_Of_RefArray, GlobSnow3_RefArray, Training_BeginDateVecList, Training_EndDateVecList)
 
-MonthlyList_CPCsoilmoist_YYYYMMDD_Of_PrcntlArray, MonthlyList_CPCsoilmoist_PrcntlArray = MonthlyList_YYYYMMDDAndArray(CPCsoilmoist_YYYYMMDD_Of_PrcntlArray, CPCsoilmoist_PrcntlArray)
+GlobSnow3_PrcntlArray = LoopPercentileCalcOverSpatialUnits(GlobSnow3_RefArray, GlobSnow3_PrcntlArray)
 
-MonthlyList_CPCsoilmoist_PrcntlArray = LoopPrcntlCalcOverMonthsNSpatialUnits(MonthlyList_CPCsoilmoist_RefArray, MonthlyList_CPCsoilmoist_PrcntlArray)
+print('Training: NumDates = ', GlobSnow3_PrcntlArray.shape[0], ', NumSpatialUnits = ',GlobSnow3_PrcntlArray.shape[1])
+PrintInfoAboutArray(GlobSnow3_YYYYMMDD_Of_PrcntlArray, 'GlobSnow3_YYYYMMDD_Of_PrcntlArray')
+PrintInfoAboutArray(GlobSnow3_PrcntlArray, 'GlobSnow3_PrcntlArray')
 
-CPCsoilmoist_PrcntlArray = ReAssembleArraysFromMonthlyList(CPCsoilmoist_YYYYMMDD_Of_PrcntlArray, CPCsoilmoist_PrcntlArray, MonthlyList_CPCsoilmoist_YYYYMMDD_Of_PrcntlArray, MonthlyList_CPCsoilmoist_PrcntlArray)
-
-PrintInfoAboutArray(CPCsoilmoist_PrcntlArray, 'GlobSnow3_PrcntlArray')
-
-
-np.savez_compressed(TrainDataFilename, YYYYMMDD_Of_Array = CPCsoilmoist_YYYYMMDD_Of_PrcntlArray, GlobSnow3_PrcntlArray = CPCsoilmoist_PrcntlArray) 
+np.savez_compressed(TrainDataFilename, 
+                    YYYYMMDD_Of_Array = GlobSnow3_YYYYMMDD_Of_PrcntlArray, 
+                    GlobSnow3_PrcntlArray = GlobSnow3_PrcntlArray) 
 
 #END section for training
 
